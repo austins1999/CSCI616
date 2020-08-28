ac3c8fd6a65744e2d40cda395a9a4526dc8092c7
austins1999 <austins@g.cofc.edu>
2020-08-28 11:32:48 -0400

    first commit

---
+++ gcd_2/target/debug/deps/gcd_2.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/CSCI616/gcd_2/target/debug/deps/gcd_2: src/main.rs
+
+/Users/sam/Documents/CSCI616/gcd_2/target/debug/deps/gcd_2.d: src/main.rs
+
+src/main.rs:
---
+++ gcd_2/target/debug/gcd_2.d
@@ -0,0 +1,1 @@
+/Users/sam/Documents/CSCI616/gcd_2/target/debug/gcd_2: /Users/sam/Documents/CSCI616/gcd_2/src/main.rs
