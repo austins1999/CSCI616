381be86a66fb445414d7df2f4e443ebbe93769e1
austins1999 <austins@g.cofc.edu>
2020-09-03 19:36:30 -0400

    a few notes from week 2

---
+++ week2/target/debug/deps/week2.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/CSCI616/week2/target/debug/deps/week2: src/main.rs
+
+/Users/sam/Documents/CSCI616/week2/target/debug/deps/week2.d: src/main.rs
+
+src/main.rs:
---
+++ week2/target/debug/week2.d
@@ -0,0 +1,1 @@
+/Users/sam/Documents/CSCI616/week2/target/debug/week2: /Users/sam/Documents/CSCI616/week2/src/main.rs
